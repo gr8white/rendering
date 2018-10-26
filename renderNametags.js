2701d1004d6b3161f481e425df79c08c98823f2d
Derrick White <gr8white@uga.edu>
2018-10-25 21:29:29 -0500

    Finished renderNametags

--- renderNametags.js
+++ renderNametags.js
@@ -1,23 +1,29 @@
-
-function renderNametags(nametags) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(nametags)}</code>
+function createSingleNametag (nametags) {
+  return `
+        <div class="text-center mt-5" style= "width: 30%;border-color: black;border-style: solid;">
+            <h1 style= "background-color: blue; color: white">Hello, my name is:</h1>
+            <h2 style="
+            padding: 20px 0px;
+            margin: 0;
+        ">${nametags}</h2>
         </div>
     `
 }
 
-function nametags() {
-    var content = document.getElementById('content');
+function renderNametags (nametags) {
+    return nametags.map(createSingleNametag).join('')
+}
 
-    var nametagsAbstraction = [
-       "Kamilah",
-       "Kim",
-       "Stuart",
-       "Ron",
-       "Krissy"
-    ]
+function nametags () {
+  var content = document.getElementById('content')
 
-    content.innerHTML = renderNametags(nametagsAbstraction);
+  var nametagsAbstraction = [
+    'Kamilah',
+    'Kim',
+    'Stuart',
+    'Ron',
+    'Krissy'
+  ]
 
+  content.innerHTML = renderNametags(nametagsAbstraction)
 }
