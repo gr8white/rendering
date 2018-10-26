312e442674c246f1922e9ddc415bb235d444c0af
Derrick White <gr8white@uga.edu>
2018-10-25 21:07:09 -0500

    Finished renderCircles

--- renderCircles.js
+++ renderCircles.js
@@ -1,36 +1,40 @@
 
-function renderCircles(circles) {
-    // HINT: You probably need to write a for loop!
-    //       Or, if you're feeling fancy, use .map() 
-    return `
+function createSingleCircle (circles) {
+  return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(circles)}</code>
+            <div style= "width: ${circles.radius}px; height: ${circles.radius}px; border-radius: ${circles.radius}px; background-color:${circles.color}"></div>
         </div>
     `
 }
 
-function circles() {
-    var content = document.getElementById('content');
+function renderCircles (circles) {
+  return circles.map(createSingleCircle).join('')
 
-    var circlesAbstraction = [
-        {
-            radius: 50,
-            color: "#FF00FF"
-        },
-        {
-            radius: 30,
-            color: "#FF99AA"
-        },
-        {
-            radius: 60,
-            color: "#0000FF"
-        },
-        {
-            radius: 10,
-            color: "#000000"
-        },
-    ];
+  // HINT: You probably need to write a for loop!
+  //       Or, if you're feeling fancy, use .map()
+}
 
-    content.innerHTML = renderCircles(circlesAbstraction);
+function circles () {
+  var contentEl = document.getElementById('content')
 
+  var circlesAbstraction = [
+    {
+      radius: 50,
+      color: '#FF00FF'
+    },
+    {
+      radius: 30,
+      color: '#FF99AA'
+    },
+    {
+      radius: 60,
+      color: '#0000FF'
+    },
+    {
+      radius: 10,
+      color: '#000000'
+    }
+  ]
+
+  contentEl.innerHTML = renderCircles(circlesAbstraction)
 }
