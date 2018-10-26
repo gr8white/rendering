30247944cb6dade1b23007d14919ec811110d262
Derrick White <gr8white@uga.edu>
2018-10-25 20:04:32 -0500

    Finished renderRectangle

--- renderRectangle.js
+++ renderRectangle.js
@@ -1,21 +1,21 @@
 
-function renderRectangle(rectangle) {
-    return `
+function renderRectangle (rectangle) {
+  return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(rectangle)}</code>
+            <div style= "width: ${rectangle.width}px; height:${rectangle.height}px; background-color:${rectangle.color}"
+            </div>
         </div>
     `
 }
 
-function rectangle() {
-    var content = document.getElementById('content');
+function rectangle () {
+  var content = document.getElementById('content')
 
-    var rectangleAbstraction = {
-        width: 100,
-        height: 200,
-        color: "#FF00FF"
-    }
+  var rectangleAbstraction = {
+    width: 100,
+    height: 200,
+    color: '#FF00FF'
+  }
 
-    content.innerHTML = renderRectangle(rectangleAbstraction);
-
+  content.innerHTML = renderRectangle(rectangleAbstraction)
 }
