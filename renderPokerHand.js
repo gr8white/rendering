3f748ef09ee87dc01b4961fe301de924c3ce69ff
Derrick White <gr8white@uga.edu>
2018-10-27 10:48:44 -0500

    stash

--- renderPokerHand.js
+++ renderPokerHand.js
@@ -1,40 +1,48 @@
-
-function renderPokerHand(pokerHand) {
-    // HINT: You can use <img /> tags that point to these playing card images: 
-    // https://commons.wikimedia.org/wiki/Category:SVG_playing_cards
-    return `
+function createSingleCard (pokerHand) {
+  return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(pokerHand)}</code>
+            <img src= 'cards/${pokerHand.value}${pokerHand.suit}.png'/>
         </div>
     `
 }
 
-function pokerHand() {
-    var content = document.getElementById('content');
+function renderPokerHand (pokerHand) {
+  // HINT: You can use <img /> tags that point to these playing card images:
+  // https://commons.wikimedia.org/wiki/Category:SVG_playing_cards
+  return pokerHand.map(createSingleCard).join('')
+}
 
-    var pokerHandAbstraction = [
-        {
-            value: "K",
-            suit: "C"
-        },
-        {
-            value: "K",
-            suit: "D"
-        },
-        {
-            value: "9",
-            suit: "S"
-        },
-        {
-            value: "2",
-            suit: "H"
-        },
-        {
-            value: "9",
-            suit: "H"
-        }
-    ];
+// function number (value) {
+//     if (value === '2') {
+//         return
+//     }
+// }
 
-    content.innerHTML = renderPokerHand(pokerHandAbstraction);
+function pokerHand () {
+  var content = document.getElementById('content')
 
+  var pokerHandAbstraction = [
+    {
+      value: 'K',
+      suit: 'C'
+    },
+    {
+      value: 'K',
+      suit: 'D'
+    },
+    {
+      value: '9',
+      suit: 'S'
+    },
+    {
+      value: '2',
+      suit: 'H'
+    },
+    {
+      value: '9',
+      suit: 'H'
+    }
+  ]
+
+  content.innerHTML = renderPokerHand(pokerHandAbstraction)
 }
