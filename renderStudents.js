0b724881028df747d5091cdd76ae7b3c968c4b4f
Derrick White <gr8white@uga.edu>
2018-10-27 00:14:39 -0500

    Finished renderStudents

--- renderStudents.js
+++ renderStudents.js
@@ -1,37 +1,58 @@
 
-function renderStudents(students) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
-        </div>
-    `
+// Creates student div
+function createSingleStudent (students) {
+  return `
+      <div id= 'wrapper' class="text-center mt-5" style= "${shade(students.isPresent)}width: 30%;border-color: black;border-style: solid;margin: 0 auto;">
+          <h1>${students.name}</h1>
+          <h2>${attendance(students.isPresent)}</h2>
+      </div>
+`
 }
 
-function students() {
-    var content = document.getElementById('content');
+function attendance (value) {
+  if (value === true) {
+    return 'Present'
+  } else if (value === false) {
+    return 'Absent'
+  }
+}
+function shade (value) {
+  if (value === true) {
+    return 'background-color: #00ff00; '
+  } else if (value === false) {
+    return 'background-color: #ff0000; '
+  }
+}
 
-    var studentsAbstraction = [
-        {
-            name: "Kamilah",
-            isPresent: true
-        },
-        {
-            name: "Kim",
-            isPresent: true
-        },
-        {
-            name: "Stuart",
-            isPresent: false
-        },
-        {
-            name: "Ron",
-            isPresent: true
-        },
-        {
-            name: "Krissy",
-            isPresent: false
-        }
-    ]
+function renderStudents (students) {
+  return students.map(createSingleStudent).join('')
+}
 
-    content.innerHTML = renderStudents(studentsAbstraction);
+function students () {
+  var content = document.getElementById('content')
+
+  var studentsAbstraction = [
+    {
+      name: 'Kamilah',
+      isPresent: true
+    },
+    {
+      name: 'Kim',
+      isPresent: true
+    },
+    {
+      name: 'Stuart',
+      isPresent: false
+    },
+    {
+      name: 'Ron',
+      isPresent: true
+    },
+    {
+      name: 'Krissy',
+      isPresent: false
+    }
+  ]
+
+  content.innerHTML = `<h1 style= "text-align: center">Roll Call!</h1>` + renderStudents(studentsAbstraction)
 }
