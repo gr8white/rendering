96fe9dcc31a0511fba77b9a6d3cfe4d484ec1a23
Derrick White <gr8white@uga.edu>
2018-10-27 12:06:03 -0500

    Finished renderRestaurants

--- renderRestaurants.js
+++ renderRestaurants.js
@@ -1,35 +1,51 @@
-
-function renderRestaurants(restaurants) {
-    // HINT: You can use <img /> tags that point to these playing card images: 
-    // https://commons.wikimedia.org/wiki/Category:SVG_playing_cards
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(restaurants)}</code>
+function createSingleRestaurant (restaurants) {
+  return `
+        <div class="text-center mt-5" style= 'background-color: rgb(218, 218, 218); width: 250px; border-radius: 25px;padding: 10px;'>
+            <h1 style= 'text-align: left'>${restaurants.name}</h1>
+            <p style= 'text-align: left;margin-bottom: 0px'>${restaurants.type}</p>
+            <h1 style= 'color: green;text-align: left'>${checkPrice(restaurants.priceRating)}</h1>
         </div>
     `
 }
 
-function restaurants() {
-    var content = document.getElementById('content');
+function checkPrice (value) {
+  if (value === 1) {
+    return '$'
+  } else if (value === 2) {
+    return '$$'
+  } else if (value === 3) {
+    return '$$$'
+  } else if (value === 4) {
+    return '$$$$'
+  } else if (value === 5) {
+    return '$$$$$'
+  }
+}
 
-    var restaurantsAbstraction = [
-        {
-            name: "McDonald's",
-            type: "Fast Food",
-            priceRating: 1
-        },
-        {
-            name: "Gunshow",
-            type: "Date Night Dining",
-            priceRating: 5
-        },
-        {
-            name: "Iron Age",
-            type: "Korean BBQ",
-            priceRating: 4
-        },
-    ];
+function renderRestaurants (restaurants) {
+  return restaurants.map(createSingleRestaurant).join('')
+}
 
-    content.innerHTML = renderRestaurants(restaurantsAbstraction);
+function restaurants () {
+  var content = document.getElementById('content')
 
+  var restaurantsAbstraction = [
+    {
+      name: "McDonald's",
+      type: 'Fast Food',
+      priceRating: 1
+    },
+    {
+      name: 'Gunshow',
+      type: 'Date Night Dining',
+      priceRating: 5
+    },
+    {
+      name: 'Iron Age',
+      type: 'Korean BBQ',
+      priceRating: 4
+    }
+  ]
+
+  content.innerHTML = renderRestaurants(restaurantsAbstraction)
 }
